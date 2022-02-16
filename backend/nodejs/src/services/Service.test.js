6d03ca70bd97208e209ec0963d15d85393e5d3c2
vitor-lucio <33261657+vitor-lucio@users.noreply.github.com>
2022-02-16 18:21:50 -0300

    criação do primeiro teste do service e uso de --verbose ao rodar jest

---
+++ backend/nodejs/src/services/Service.test.js
@@ -0,0 +1,17 @@
+describe('Testando ASobreB', () => {
+    const service = require('./Service');
+
+    describe('Quando 2 números são passados como parâmetro, retorna a divisão de um pelo outro', () => {
+        test.concurrent.each`
+           numerador                        | denominador                        | valorEsperado
+            ${6}                            |  ${3}                              |    ${2}
+            ${4}                            |  ${2}                              |    ${2}
+            ${10}                           |  ${3}                              |    ${3.3333333333333335}
+            ${Number.MAX_SAFE_INTEGER}      |  ${5}                              |    ${1801439850948198.2}
+            ${1}                            |  ${Number.MAX_SAFE_INTEGER}        |    ${1.1102230246251568e-16}
+        `('.ASobreB($numerador, $denominador)', async ({numerador, denominador, valorEsperado}) => {
+            expect(service.ASobreB(numerador, denominador)).toBe(valorEsperado);
+        });
+    });
+
+});

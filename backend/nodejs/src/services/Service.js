bfc5ce1189464db62b490c4327ab1b12eed58032
Laura Godinho <lauragbarroso1600@gmail.com>
2022-03-02 20:03:29 -0300

    função updateClassificacaoEtariaById e outras mudanças necessárias

--- backend/nodejs/src/services/Service.js
+++ backend/nodejs/src/services/Service.js
@@ -114,12 +114,43 @@ class Service {
     await user.delete();
   }
 
+
+  /**
+    * Retorna uma string com uma lógica interna sobre o que essa string pode ser
+    */
   async getNameById(id){
     const user = await User.findByPk(id);
-    if( user.name[0]==='a' ||  user.name[0]==='e' ||  user.name[0]==='i' || user.name[0]==='o' || user.name[0] === 'u' ){
-      return `O nome do usuário começa com vogal ("${user.name}").`;
+    if( user.name[0]==='a' || user.name[0]==='e' || user.name[0]==='o' || user.name[0] === 'u' ){
+      return "O nome do usuário começa com vogal (`${user.name}`).";
     }
-    return `O nome do usuário começa com consoante ("${user.name}").`;
+    return "O nome do usuário começa com consoante (`${user.name}`).";
+  }
+
+  /**
+    * Retorna um objeto com valores preenchidos internamente
+    */
+  async updateClassificacaoEtariaById(id){
+    const user = await User.findByPk(id);
+    let nova_classificacao_etaria;
+
+    if (!user) {
+      throw new NotAuthorizedError(
+        `Nao foi encontrado um usuario com o ID: ${id}`
+      );
+    }
+
+    if(user.age>=0 && user.age<=12 ){
+      nova_classificacao_etaria = "crianca";
+    } else if(user.age>12 && user.age<18){
+      nova_classificacao_etaria = "adolescente";
+    } else if(user.age>=18){
+      nova_classificacao_etaria = "adulto";
+    }
+
+    user.classificacao_etaria = nova_classificacao_etaria;
+    user.update({classificacao_etaria:nova_classificacao_etaria});
+
+    return user;
   }
 
 }

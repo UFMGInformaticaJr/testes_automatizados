dbd5965f9e7d049a68c59977746d2bfc7ee6d04c
vitor-lucio <33261657+vitor-lucio@users.noreply.github.com>
2022-04-02 12:40:21 -0300

    refatoração de nomes nos testes do error-handler

--- backend/nodejs/src/middlewares/error-handler.test.js
+++ backend/nodejs/src/middlewares/error-handler.test.js
@@ -11,38 +11,38 @@ describe('error-handler', () => {
 
   test.each([
     {
-      err:new NotAuthorizedError('Teste'), 
+      erroLancado:new NotAuthorizedError('Teste'), 
       statusEsperado: 401, 
       mensagemEsperada:{error: 'Teste'}
     },
     {
-      err:new AccessDeniedError('Teste message'), 
+      erroLancado:new AccessDeniedError('Teste message'), 
       statusEsperado: 403, 
       mensagemEsperada: {error: 'Teste message'}
     },
     {
-      err:new InternalError('Internal error!'), 
+      erroLancado:new InternalError('Internal error!'), 
       statusEsperado: 500, 
       mensagemEsperada: {error: 'Internal error!'}
     },
     {
-      err:new InvalidParamsError('Message'),
+      erroLancado:new InvalidParamsError('Message'),
       statusEsperado: 400, 
       mensagemEsperada: {error: 'Message'}
     },
     {
-      err:new MediaTypeError('Something went wrong!'), 
+      erroLancado:new MediaTypeError('Something went wrong!'), 
       statusEsperado: 415, 
       mensagemEsperada: {error: 'Something went wrong!'}
     },
     {
-      err:new NotFoundError("The resource couldn't be found"), 
+      erroLancado:new NotFoundError("The resource couldn't be found"), 
       statusEsperado: 404, 
       mensagemEsperada: {error: "The resource couldn't be found"}
     },
   ])(
     '%j',
-    ({err, statusEsperado, mensagemEsperada}) => {
+    ({erroLancado, statusEsperado, mensagemEsperada}) => {
       /*
        mockReturnThis torna possível o encadeamento de chamadas usado em 
        res.status(s).json(j)
@@ -56,7 +56,7 @@ describe('error-handler', () => {
       Os atributos req e next são nulos pois não importa para o objetivo 
       deste teste
       */
-      errorHandler(err, null, res, null);
+      errorHandler(erroLancado, null, res, null);
       expect(res.status).toBeCalledWith(statusEsperado);
       expect(res.json).toBeCalledWith(mensagemEsperada);
     }

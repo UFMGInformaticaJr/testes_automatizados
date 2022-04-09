d77d110ac2e92c644ce2001e1597f6899f551c43
vitor-lucio <33261657+vitor-lucio@users.noreply.github.com>
2022-04-09 18:24:32 -0300

    Adicionada as descrições dos testes dos middlewares

--- backend/nodejs/src/middlewares/error-handler.test.js
+++ backend/nodejs/src/middlewares/error-handler.test.js
@@ -8,57 +8,58 @@ describe('error-handler', () => {
     MediaTypeError,
     NotFoundError,
   } = require('../errors');
-
-  test.each([
-    {
-      erroLancado:new NotAuthorizedError('Teste'), 
-      statusEsperado: 401, 
-      mensagemEsperada:{error: 'Teste'}
-    },
-    {
-      erroLancado:new AccessDeniedError('Teste message'), 
-      statusEsperado: 403, 
-      mensagemEsperada: {error: 'Teste message'}
-    },
-    {
-      erroLancado:new InternalError('Internal error!'), 
-      statusEsperado: 500, 
-      mensagemEsperada: {error: 'Internal error!'}
-    },
-    {
-      erroLancado:new InvalidParamsError('Message'),
-      statusEsperado: 400, 
-      mensagemEsperada: {error: 'Message'}
-    },
-    {
-      erroLancado:new MediaTypeError('Something went wrong!'), 
-      statusEsperado: 415, 
-      mensagemEsperada: {error: 'Something went wrong!'}
-    },
-    {
-      erroLancado:new NotFoundError("The resource couldn't be found"), 
-      statusEsperado: 404, 
-      mensagemEsperada: {error: "The resource couldn't be found"}
-    },
-  ])(
-    '%j',
-    ({erroLancado, statusEsperado, mensagemEsperada}) => {
-      /*
-       mockReturnThis torna possível o encadeamento de chamadas usado em 
-       res.status(s).json(j)
-       */
-      const res = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn().mockReturnThis(),
-      };
-
-      /*
-      Os atributos req e next são nulos pois não importa para o objetivo 
-      deste teste
-      */
-      errorHandler(erroLancado, null, res, null);
-      expect(res.status).toBeCalledWith(statusEsperado);
-      expect(res.json).toBeCalledWith(mensagemEsperada);
-    }
-  );
+  describe('erro é recebido ==> lança um código e mensagem como resposta', () => {
+    test.each([
+      {
+        erroLancado:new NotAuthorizedError('Teste'), 
+        statusEsperado: 401, 
+        mensagemEsperada:{error: 'Teste'}
+      },
+      {
+        erroLancado:new AccessDeniedError('Teste message'), 
+        statusEsperado: 403, 
+        mensagemEsperada: {error: 'Teste message'}
+      },
+      {
+        erroLancado:new InternalError('Internal error!'), 
+        statusEsperado: 500, 
+        mensagemEsperada: {error: 'Internal error!'}
+      },
+      {
+        erroLancado:new InvalidParamsError('Message'),
+        statusEsperado: 400, 
+        mensagemEsperada: {error: 'Message'}
+      },
+      {
+        erroLancado:new MediaTypeError('Something went wrong!'), 
+        statusEsperado: 415, 
+        mensagemEsperada: {error: 'Something went wrong!'}
+      },
+      {
+        erroLancado:new NotFoundError("The resource couldn't be found"), 
+        statusEsperado: 404, 
+        mensagemEsperada: {error: "The resource couldn't be found"}
+      },
+    ])(
+      '%j',
+      ({erroLancado, statusEsperado, mensagemEsperada}) => {
+        /*
+         mockReturnThis torna possível o encadeamento de chamadas usado em 
+         res.status(s).json(j)
+         */
+        const res = {
+          status: jest.fn().mockReturnThis(),
+          json: jest.fn().mockReturnThis(),
+        };
+  
+        /*
+        Os atributos req e next são nulos pois não importa para o objetivo 
+        deste teste
+        */
+        errorHandler(erroLancado, null, res, null);
+        expect(res.status).toBeCalledWith(statusEsperado);
+        expect(res.json).toBeCalledWith(mensagemEsperada);
+      }
+    );
+  });
 });
--- backend/nodejs/src/middlewares/object-filter.test.js
+++ backend/nodejs/src/middlewares/object-filter.test.js
@@ -1,45 +1,47 @@
 describe('object-filter', () => {
   const {requestFilter} = require('./object-filter');
   
-  test.each([
-    {
-      reqObj:{body: {a: 1, b: 3, c: 3, d: 7}},
-      reqKey: 'body',
-      filter: ['a', 'b', 'd'],
-      valorEsperado: {body: {a: 1, b: 3, d: 7}},
-    },
-    {
-      reqObj:{body: {a: 1, b: 3, c: 3, d: 7}, query: {a: 1, b: 1}},
-      reqKey: 'body',
-      filter: ['a', 'b', 'd'],
-      valorEsperado: {body: {a: 1, b: 3, d: 7}, query: {a: 1, b: 1}},
-    },
-    {
-      reqObj:{body: {a: 1, b: 3, c: 3, d: 7}, query: {a: 1, b: 1}},
-      reqKey: 'body',
-      filter: ['a', 'b', 'e'],
-      valorEsperado: {body: {a: 1, b: 3}, query: {a: 1, b: 1}},
-    },
-    {
-      reqObj:{body: {a: 1, b: 3, c: 3, d: 7}, query: {a: 1, b: 1}},
-      reqKey: 'query',
-      filter: ['a'],
-      valorEsperado: {body: {a: 1, b: 3, c: 3, d: 7}, query: {a: 1}},
-    },
-    {
-      reqObj:{query: {a: 1, b: 1}},
-      reqKey: 'query',
-      filter: ['a'],
-      valorEsperado: {query: {a: 1}},
-    },
-  ])('%j', ({reqObj, reqKey, filter, valorEsperado}) => {
-    const mockedNext = jest.fn();
-    const reqFilter = requestFilter(reqKey, filter);
-
-    // O atributo res é nulo pois não importa para o objetivo deste teste
-    reqFilter(reqObj, null, mockedNext);
-
-    expect(mockedNext).toBeCalledWith();
-    expect(reqObj).toEqual(valorEsperado);
+  describe('object-filter recebe o body de uma requisição ==> body é filtrado', () => {
+    test.each([
+      {
+        reqObj:{body: {a: 1, b: 3, c: 3, d: 7}},
+        reqKey: 'body',
+        filter: ['a', 'b', 'd'],
+        valorEsperado: {body: {a: 1, b: 3, d: 7}},
+      },
+      {
+        reqObj:{body: {a: 1, b: 3, c: 3, d: 7}, query: {a: 1, b: 1}},
+        reqKey: 'body',
+        filter: ['a', 'b', 'd'],
+        valorEsperado: {body: {a: 1, b: 3, d: 7}, query: {a: 1, b: 1}},
+      },
+      {
+        reqObj:{body: {a: 1, b: 3, c: 3, d: 7}, query: {a: 1, b: 1}},
+        reqKey: 'body',
+        filter: ['a', 'b', 'e'],
+        valorEsperado: {body: {a: 1, b: 3}, query: {a: 1, b: 1}},
+      },
+      {
+        reqObj:{body: {a: 1, b: 3, c: 3, d: 7}, query: {a: 1, b: 1}},
+        reqKey: 'query',
+        filter: ['a'],
+        valorEsperado: {body: {a: 1, b: 3, c: 3, d: 7}, query: {a: 1}},
+      },
+      {
+        reqObj:{query: {a: 1, b: 1}},
+        reqKey: 'query',
+        filter: ['a'],
+        valorEsperado: {query: {a: 1}},
+      },
+    ])('%j', ({reqObj, reqKey, filter, valorEsperado}) => {
+      const mockedNext = jest.fn();
+      const reqFilter = requestFilter(reqKey, filter);
+  
+      // O atributo res é nulo pois não importa para o objetivo deste teste
+      reqFilter(reqObj, null, mockedNext);
+  
+      expect(mockedNext).toBeCalledWith();
+      expect(reqObj).toEqual(valorEsperado);
+    });
   });
 });

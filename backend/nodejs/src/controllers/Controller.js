18ab235842bd1aefe45969863658f51abe8ad475
vitor-lucio <33261657+vitor-lucio@users.noreply.github.com>
2022-05-01 20:31:13 -0300

    fix em um require de uma classe que nao existe mais e removendo algumas rotas

--- backend/nodejs/src/controllers/Controller.js
+++ backend/nodejs/src/controllers/Controller.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 
-const Service = require('../services/Service');
+const operacoesAritmeticasService = require('../services/OperacoesAritmeticasService');
+const stringService = require('../services/StringService');
+const senhaService = require('../services/SenhaService');
 
 router.get('/raizQuadrada/:numero', function (req, res) {
   const numero = req.params.numero;
   try {
-    Service.raizQuadrada(numero);
+    operacoesAritmeticasService.raizQuadrada(numero);
     res.end();
   } catch (error) {
     console.log(error + ': Valor Inválido');
@@ -16,7 +18,7 @@ router.get('/raizQuadrada/:numero', function (req, res) {
 router.get('/divisores/:numero', function (req, res) {
   const numero = req.params.numero;
   try {
-    Service.divisores(numero);
+    operacoesAritmeticasService.divisores(numero);
     res.end();
   } catch (error) {
     console.log(error + ': Valor Inválido');
@@ -27,7 +29,7 @@ router.get('/divisores/:numero', function (req, res) {
 router.get('/vogais/:string', function (req, res) {
   const string = req.params.string;
   try {
-    Service.vogais(string);
+    stringService.vogais(string);
     res.end();
   } catch (error) {
     console.log(error + ': Valor Inválido');
@@ -38,27 +40,7 @@ router.get('/vogais/:string', function (req, res) {
 router.get('/senhaFraca/:id', function (req, res) {
   try {
     const userId = req.params.id;
-    Service.senhaFraca(userId);
-    res.end();
-  } catch (error) {
-    console.log(error);
-    res.end();
-  }
-});
-
-router.get('/getAllUsers/', function (req, res) {
-  try {
-    Service.getAllUsers();
-    res.end();
-  } catch (error) {
-    console.log(error);
-    res.end();
-  }
-});
-
-router.get('/retornaUsuariosComSenhaFraca/', function (req, res) {
-  try {
-    Service.retornaUsuariosComSenhaFraca();
+    senhaService.senhaFraca(userId);
     res.end();
   } catch (error) {
     console.log(error);
